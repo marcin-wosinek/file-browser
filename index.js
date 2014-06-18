8d6763d690d4f367aed2b4c87ccbe8c7d1c9c073
Marcin Wosinek <marcin.wosinek@contractors.roche.com>
2014-06-18 16:01:33 +0200

    add voting backend

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
-var //fs = require("fs")
-   port = process.argv[2] || 1107
+var fs = require('fs')
+  , port = process.argv[2] || 1107
   , express = require('express')
+  , _ = require('underscore')
   , serveIndex = require('serve-index')
-  , app = express();
+  , app = express()
+  , store = require("jfs")
+  , db = new store("data");
 
-app.use('/files', express.static(__dirname + '/files'));
+var FILES_DIR = '/files';
 
-app.use('/files', serveIndex(__dirname + '/files'));
+app.use('/files', express.static(__dirname +FILES_DIR));
+
+app.use('/files', serveIndex(__dirname +FILES_DIR));
 
 app.use('/bower_components', express.static(__dirname + '/static/bower_components'));
 app.use(express.static(__dirname + '/static/app'));
 
-app.use(function(request, response) {
+var router = express.Router();
+
+router.get('/files', function (request, response) {
+
+  fs.readdir(__dirname + FILES_DIR, function (err, files) {
+    var objArray = _.map(files, function (fileName) { return { name: fileName}});
+    db.all(function (err, value) {
+
+      if (!value) {
+        value = {};
+      }
+
+      var array = _.map(objArray, function (fileObj) {
+        return _.extend(fileObj, {rate: value[fileObj.name]});
+      });
+
+      response.json(array);
+    });
+  });
+});
+
+var changeVotes = function (fileName, change, response) {
+  db.get(fileName, function(err, value){
+    value += change;
+    db.save(fileName, value, function(err, obj){
+      response.json({result: "success"});
+    });
+  });
+};
+
+router.post('/downvote/:file', function (request, response) {
+  var fileName = request.params.file;
+
+  changeVotes(fileName, -1, response);
+});
+
+router.post('/upvote/:file', function (request, response) {
+  var fileName = request.params.file;
+
+  changeVotes(fileName, 1, response);
+});
+
+app.use('/api', router);
+
+app.use(function (request, response) {
   redirect('/index.html');
 });
 
